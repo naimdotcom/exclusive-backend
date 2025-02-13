79488df8a2e9d02c1745473612b7c05649426476
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-02-13 22:38:13 +0600

    add cart model; define schema for user, product, quantity, and totalPrice

---
+++ src/Model/cart.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const { Schema, Types, model } = mongoose;
+
+const cartSchema = new Schema({
+  user: {
+    type: Types.ObjectId,
+    ref: "user",
+    require: true,
+  },
+  product: {
+    type: Types.ObjectId,
+    ref: "product",
+    require: true,
+  },
+  quantity: {
+    type: Number,
+    require: true,
+    default: 1,
+  },
+  totalPrice: {
+    type: Number,
+  },
+});
