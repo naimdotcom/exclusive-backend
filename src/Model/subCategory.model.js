d3f07aead553cbad084669ef5843e103cb4d5c5d
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-03-20 05:16:45 +0600

    enhance subCategory model; add timestamps option to schema and fix category reference

--- src/Model/subCategory.model.js
+++ src/Model/subCategory.model.js
@@ -2,26 +2,31 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const subCatgorySchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    require: true,
-  },
-  image: {
-    type: String,
-  },
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: "category",
-  },
-  products: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "product",
+const subCatgorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: true,
     },
-  ],
-});
+    image: {
+      type: String,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "category",
+    },
+    products: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const subCategory = mongoose.model("subCategory", subCatgorySchema);
 
